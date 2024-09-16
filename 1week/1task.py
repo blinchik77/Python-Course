295c696a43df776a4635ed8d1421f7a7d6d03040
blinchik77 <nvblinov@edu.hse.ru>
2024-09-17 00:37:07 +0300

    some changes in task1 file

---
+++ 1week/1task.py
@@ -0,0 +1,14 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/multiply-strings/description/
+"""
+
+
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+print("hello")
