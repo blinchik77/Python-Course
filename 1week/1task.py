cdfb188afdb051d6f41293aab928c09f09244721
blinchik77 <nvblinov@edu.hse.ru>
2024-09-22 14:06:06 +0300

    solved 1task

--- 1week/1task.py
+++ 1week/1task.py
@@ -1,14 +1,21 @@
 """
 leetcode.com/problem-list/string/
-url: https://leetcode.com/problems/multiply-strings/description/
+url: https://leetcode.com/problems/longest-substring-without-repeating-characters
 """
 
 
 class Solution(object):
-    def multiply(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
-print("hello")
+    def lengthOfLongestSubstring(self,s: str) -> int:
+
+        left_pointer = 0
+        current_res = []
+        best_res = 0
+
+        for right_pointer in range(len(s)):
+            while s[right_pointer] in current_res:
+                left_pointer+=1
+                current_res.pop(0)
+
+            current_res.append(s[right_pointer])
+            best_res = max(best_res, right_pointer - left_pointer + 1)
+        return best_res
---
+++ 1week/2task.py
@@ -0,0 +1,7 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/largest-number/
+"""
+class Solution(object):
+    def largestNumber(self, nums):
+        nums.sort()
