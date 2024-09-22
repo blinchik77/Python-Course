1e721a0495e6e76e9540854d28e87abafea03b75
blinchik77 <nvblinov@edu.hse.ru>
2024-09-22 19:14:32 +0300

    Solve 5task

---
+++ 1week/5task.py
@@ -0,0 +1,26 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/reverse-words-in-a-string
+"""
+class Solution(object):
+    def reverseWords(self, s):
+
+        res = ""
+        current_word = ""
+
+
+        for i in range(len(s) - 1, - 1,-1):
+            if (s[i] != " "):
+                current_word += s[i]
+            else:
+                if current_word:
+                    current_word = current_word[::-1] + " "
+                    res+= current_word
+                    current_word = ""
+
+        res += current_word[::-1]
+        if res[-1] == " ":
+            res = res[:-1]
+        return res
+
+solution = Solution()
