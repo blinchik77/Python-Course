21e39eae825cf40b17d3742a7ca78cfbc252e210
blinchik77 <nvblinov@edu.hse.ru>
2024-09-29 22:32:09 +0300

    3task solved

--- 2week/3.py
+++ 2week/3.py
@@ -1,1 +1,20 @@
+"""
+leetcode.com/problem-list/string/
+url: https://leetcode.com/problems/word-break
+"""
 
+
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        word_set = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+
+        return dp[n]
